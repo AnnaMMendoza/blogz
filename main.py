290a9246c0d19c4c67b72cb5f6d83314c75c0258
Anna M Mendoza <annammendoza@gmail.com>
2018-11-30 19:43:01 -0600

    added login and register handlers and verifications

--- main.py
+++ main.py
@@ -27,7 +27,7 @@ class User(db.Model):
     password = db.Column(db.String(15))
     blogs = db.relationship('Blog', backref='owner')
 
-    def __init__(self, id, username, password, blogz):
+    def __init__(self, username, password):
         self.username = username
         self.password = password
 
@@ -40,38 +40,86 @@ class User(db.Model):
     
 @app.route('/login', methods=['POST', 'GET'])
 def login():
+
+    username_error = ""
+    pass_error = ""
+
     if request.method == 'POST':
         username = request.form['username']
         password = request.form['password']
         user = User.query.filter_by(username=username).first()
+
         if user and user.password == password: # searches for the username in the database
             session['username'] = username # stores the username in the session
-            flash("Logged in")
-            return redirect('/blog')
-        else:
-            flash('User password incorrect, or user does not exist', 'error')
+            flash("You are logged in!")
+            return redirect('/newpost')
+        
+        if not user:
+            username_error = "Username does not exist"
 
-    return render_template('login.html')
+        if user and user.password != password:
+            pass_error = "Password is incorrect"           
 
-# @app.route('/register', methods=['POST', 'GET'])
-# def register():
-#     if request.method == 'POST':
-#         username = request.form['username']
-#         password = request.form['password']
-#         verify = request.form['verify']
+    return render_template('login.html', username_error=username_error, pass_error=pass_error)
 
-#         # validation goes here!
+@app.route('/register', methods=['POST', 'GET'])
+def register():
 
+    username_error = ""
+    pass_error = ""
+    verifypw_error = ""
+    username = ""
 
-#         existing_user = User.query.filter_by(username=username).first()
-#         if not existing_user:
-#             new_user = User(username, password)
-#             db.session.add(new_user)
-#             db.session.commit()
-#             session['username'] = username
-#             return redirect('/newpost')
-#         else:
-#             return flash("Duplicate User")
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        verify = request.form['verify']
+
+        # validation tests go here!
+        if not username:
+            username_error = "Please enter a Username"
+
+        if " " in username:
+            username_error = "No spaces allowed in Username"
+
+        if len(username) < 3 or len(username) > 20:
+            username_error = "Username length must be > 3 and < 20"
+
+    # check for duplicate username
+        existing_user = User.query.filter_by(username=username).first()
+        if existing_user:
+            username_error = "Username already exists, please choose another username"
+            return render_template('register.html', username_error)
+
+    # password validation tests
+        if password == "":
+            pass_error = "Please enter a password"
+
+        if " " in password:
+            pass_error = "Passwords may not contain spaces"
+
+        if len(password) < 3 or len(password) > 20:
+            pass_error = "Password length must be > 3 and < 20 characters"
+
+    # password verify field MUST match password field
+        if verify != password:
+            verifypw_error = "Password entries do not match, please re-enter"
+            verify = ""
+
+        if " " in password:
+            verifypw_error = "Passwords may not contain spaces"
+            verify = ""
+
+        existing_user = User.query.filter_by(username=username).first()
+
+        if not username_error and not pass_error and not verifypw_error:
+            new_user = User(username, password)
+            db.session.add(new_user)
+            db.session.commit()
+            session['username'] = username
+            return redirect('/newpost')
+
+    return render_template("register.html", username=username, username_error=username_error, pass_error=pass_error, verifypw_error=verifypw_error)
 
 
 @app.route('/', methods=['POST', 'GET']) # directs to the main blog posts page
@@ -80,6 +128,8 @@ def all_posts():
 
 @app.route('/newpost', methods=['POST', 'GET'])
 def add_post(): # adds the post to the database, new post acceptance then redirects to that blog post's individual entry page
+
+    owner = User.query.filter_by(username=session['username']).first()
 
     new_post = None
     title = ""
